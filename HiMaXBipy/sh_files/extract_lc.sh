98e0f57c4f4ed3ebb8cd7573bcea805b3b51c2b4
David Kaltenbrunner <kald@ds52.mpe.mpg.de>
2022-08-23 10:55:13 +0200

    added lc extraction

---
+++ HiMaXBipy/sh_files/extract_lc.sh
@@ -0,0 +1,47 @@
+source /home/erosita/sw/eSASSusers_201009/bin/esass-init.sh
+
+PAT=all
+
+ra=right_ascension
+dec=declination
+
+src=source_name
+
+main_dir=main_name
+product_dir=result_dir
+
+region=region_code
+
+sources=sources_list
+
+#full light curve, 1s bin, TMall
+srctool eventfiles="${sources}" \
+        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_1.0s" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 5 6 7" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="1.0" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        gtitype="GTI" psftype="2D_PSF" clobber="yes"
+
+#full light curve, 1s bin, TM12346
+srctool eventfiles="${sources}" \
+        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMon_1.0s" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 6" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="1.0" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        gtitype="GTI" psftype="2D_PSF" clobber="yes"
+
+
+
+for j in 1 2 3 4 6
+do
+srctool eventfiles="${sources}" \
+        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMon_1.0s_${j}20" suffix=".fits" todo="LC LCCORR" insts="${j}" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="1.0" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        gtitype="GTI" psftype="2D_PSF" clobber="yes"
+done
+fi
+
+for j in 5 7
+do
+srctool eventfiles="${sources}" \
+        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMoff_1.0s_${j}20" suffix=".fits" todo="LC LCCORR" insts="${j}" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="1.0" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        gtitype="GTI" psftype="2D_PSF" clobber="yes"
+done
+fi
