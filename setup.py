1af496df2cc13fc343118db121d196827f774db7
David Kaltenbrunner <kald@ds52.mpe.mpg.de>
2022-09-06 02:40:39 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 	'HiMaXBipy/spectral_analysis',
 
     ],
-    version='v0.3.15',
+    version='v0.3.16',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
