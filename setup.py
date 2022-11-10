c06f3fb1cff08f515a4bd42075aa45b09c474ea6
DaKalt <kald@mpe.mpg.de>
2022-11-10 15:24:29 +0100

    some weird error

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'HiMaXBipy/spectral_analysis',
 
     ],
-    version='v1.0.10',
+    version='v1.0.11',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
