78c10470ace09410b0f86c649b1235238d4dd893
DaKalt <kald@mpe.mpg.de>
2022-11-11 12:30:07 +0100

    debugging attempt broken_axis

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'HiMaXBipy/spectral_analysis',
 
     ],
-    version='v1.0.20',
+    version='v1.0.21',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
