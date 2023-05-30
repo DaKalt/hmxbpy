66ec58a3ef1d488be2c3a9479a6987bd37e4c17e
David Kaltenbrunner <39954736+DaKalt@users.noreply.github.com>
2023-05-30 12:20:58 +0200

    Update extract_lc.sh
    bug in lc extraction (fi without if)

--- HiMaXBipy/sh_files/extract_lc.sh
+++ HiMaXBipy/sh_files/extract_lc.sh
@@ -42,7 +42,6 @@ srctool eventfiles="${sources}" \
         srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes"
 done
-fi
 
 for j in 5 7
 do
@@ -51,4 +50,3 @@ srctool eventfiles="${sources}" \
         srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes"
 done
-fi
