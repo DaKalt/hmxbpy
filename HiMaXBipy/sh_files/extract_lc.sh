5eec11c09644400f4bb4384eebd35b51b5e8304a
DaKalt <kald@mpe.mpg.de>
2023-07-29 15:14:48 +0200

    fixed a bug in LC extraction wrong E limits

--- HiMaXBipy/sh_files/extract_lc.sh
+++ HiMaXBipy/sh_files/extract_lc.sh
@@ -24,19 +24,19 @@ binning=@binning
 #full light curve, 1s bin, TMall
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_${binning}s_${emin}keV_${emax}keV_" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 5 6 7" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="${emin}" lcemax="${emax}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes"
 
 #full light curve, 1s bin, TM12346
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMon_${binning}s_${emin}keV_${emax}keV_" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 6" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="${emin}" lcemax="${emax}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes" writeinsts="0"
 
 #full light curve, 1s bin, TM57
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMoff_${binning}s_${emin}keV_${emax}keV_" suffix=".fits" todo="LC LCCORR" insts="5 7" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="${emin}" lcemax="${emax}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes" writeinsts="0"
 
 
@@ -45,7 +45,7 @@ srctool eventfiles="${sources}" \
 #do
 #srctool eventfiles="${sources}" \
 #        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMon_${binning}s_${emin}keV_${emax}keV_${j}20_" suffix=".fits" todo="LC LCCORR" insts="${j}" \
-#        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+#        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="${emin}" lcemax="${emax}" tstep=0.05 xgrid="0.5 1.0" \
 #        gtitype="GTI" psftype="2D_PSF" clobber="yes"
 #done
 #
@@ -53,6 +53,6 @@ srctool eventfiles="${sources}" \
 #do
 #srctool eventfiles="${sources}" \
 #        srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMoff_${binning}s_${emin}keV_${emax}keV_${j}20_" suffix=".fits" todo="LC LCCORR" insts="${j}" \
-#        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="0.2" lcemax="8" tstep=0.05 xgrid="0.5 1.0" \
+#        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR+" lcpars="${binning}" lcemin="${emin}" lcemax="${emax}" tstep=0.05 xgrid="0.5 1.0" \
 #        gtitype="GTI" psftype="2D_PSF" clobber="yes"
 #done
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'HiMaXBipy/spectral_analysis',
 
     ],
-    version='v1.0.77',
+    version='v1.0.78',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
