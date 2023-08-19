750fc18dd1b7d03ca4c46a103016618ba0993bfe
DaKalt <kald@mpe.mpg.de>
2023-08-19 15:43:25 +0200

    missing setup

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'HiMaXBipy/tex_style'
 
     ],
-    version='v1.0.101',
+    version='v1.0.102',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
