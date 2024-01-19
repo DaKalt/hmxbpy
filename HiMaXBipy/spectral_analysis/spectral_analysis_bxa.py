b78a2fc4831bff45639c26b8d8ffd9c99b73b544
DaKalt <kald@mpe.mpg.de>
2024-01-19 18:20:58 +0100

    debugging

--- HiMaXBipy/spectral_analysis/spectral_analysis_bxa.py
+++ HiMaXBipy/spectral_analysis/spectral_analysis_bxa.py
@@ -427,9 +427,12 @@ def write_tex(tex_file, tex_info, abs_F, unabs_L, analyser, quantiles):
 
             mask = cumsumweights > 1e-4
 
-            lower = corner.quantile(data[mask], quantiles[0], weights[mask])
-            median = corner.quantile(data[mask], quantiles[1], weights[mask])
-            upper = corner.quantile(data[mask], quantiles[2], weights[mask])
+            lower = corner.quantile(data[mask], quantiles[0]/100.,
+                                    weights[mask])
+            median = corner.quantile(data[mask], quantiles[1]/100/,
+                                     weights[mask])
+            upper = corner.quantile(data[mask], quantiles[2]/100.,
+                                    weights[mask])
             if tex_info[j][3] == 'log':
                 lower = 10 ** lower
                 median = 10 ** median
