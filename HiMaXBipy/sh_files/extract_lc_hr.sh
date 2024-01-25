cfad9dcda337e6b53d9ea09e0fe702d544c33751
DaKalt <kald@mpe.mpg.de>
2024-01-25 13:25:26 +0100

    debugging HR LC

--- HiMaXBipy/sh_files/extract_lc_hr.sh
+++ HiMaXBipy/sh_files/extract_lc_hr.sh
@@ -28,19 +28,19 @@ binning=@binning
 #full light curve, 1s bin, TMall
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_${binning}s_HR_" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 5 6 7" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0 emax1 emax2}" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0} ${emax1} ${emax2}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes" writeinsts="0"
 
 #full light curve, 1s bin, TM12346
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMon_${binning}s_HR_" suffix=".fits" todo="LC LCCORR" insts="1 2 3 4 6" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0 emax1 emax2}" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0} ${emax1} ${emax2}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes" writeinsts="0"
 
 #full light curve, 1s bin, TM57
 srctool eventfiles="${sources}" \
         srccoord="J2000;$ra,$dec" prefix="${product_dir}/${src}_${region}_eROSITA_PAT${PAT}_TMoff_${binning}s_HR_" suffix=".fits" todo="LC LCCORR" insts="5 7" \
-        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0 emax1 emax2}" tstep=0.05 xgrid="0.5 1.0" \
+        srcreg="${main_dir}/src.reg" backreg="${main_dir}/bkg.reg" exttype="POINT" lctype="REGULAR-" lcpars="${binning}" lcemin="${emin0} ${emin1} ${emin2}" lcemax="${emax0} ${emax1} ${emax2}" tstep=0.05 xgrid="0.5 1.0" \
         gtitype="GTI" psftype="2D_PSF" clobber="yes" writeinsts="0"
 
 
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'HiMaXBipy/json_files',
 
     ],
-    version='v1.0.244',
+    version='v1.0.245',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
