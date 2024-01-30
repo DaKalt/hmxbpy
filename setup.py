ec045f7855d61599c36f1537dcff4bdda46eb524
DaKalt <kald@mpe.mpg.de>
2024-01-30 17:42:45 +0100

    debugging spectral fit

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'HiMaXBipy/json_files',
 
     ],
-    version='v1.0.273',
+    version='v1.0.274',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
