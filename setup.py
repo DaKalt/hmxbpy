2e7f2a5f28f3d9a273b06a247e97a214027600da
DaKalt <kald@mpe.mpg.de>
2024-02-06 21:45:08 +0100

    debugging RGB creation

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.304',
+    version='v1.0.305',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
