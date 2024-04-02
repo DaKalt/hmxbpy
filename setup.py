a46e2507f872f6fe1555f7b5ec3bd000696f9577
DaKalt <kald@mpe.mpg.de>
2024-04-02 13:33:41 +0200

    another test

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.328',
+    version='v1.0.329',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
