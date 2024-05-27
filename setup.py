1dec75ab4954697c39c7488773ea7dd9651a67b4
DaKalt <kald@mpe.mpg.de>
2024-05-27 13:35:39 +0200

    debugging bxa plotting

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.348',
+    version='v1.0.349',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
