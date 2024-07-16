0dd9b0fadb7ee98d8f52da722598e642f83bf51b
DaKalt <kald@mpe.mpg.de>
2024-07-16 12:42:30 +0200

    debugging snr fit

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.391',
+    version='v1.0.392',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
