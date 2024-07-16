5bb94e0f47ad41961b9f5e27f3d453f647ca62c3
DaKalt <kald@mpe.mpg.de>
2024-07-16 02:48:05 +0200

    adjusting background extraction

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.389',
+    version='v1.0.390',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
