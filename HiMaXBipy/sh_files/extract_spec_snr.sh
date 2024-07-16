a7de61d1c69ee9f8ae97654a7a59096ad381a5ad
DaKalt <kald@mpe.mpg.de>
2024-07-16 02:49:37 +0200

    change back

--- HiMaXBipy/sh_files/extract_spec_snr.sh
+++ HiMaXBipy/sh_files/extract_spec_snr.sh
@@ -18,7 +18,7 @@ cd $product_dir
 
 # spectra on chip detectors
 srctool eventfiles="${sources}" \
-        prefix="${product_dir}/${epoch}_bxa_bkg_" srccoord="fk5;$ra,$dec" srcreg="${main_dir}/bkg_snr.reg" backreg="${main_dir}/src.reg" exttype="POINT" \
+        prefix="${product_dir}/${epoch}_bxa_bkg_" srccoord="fk5;$ra,$dec" srcreg="${main_dir}/bkg.reg" backreg="${main_dir}/src.reg" exttype="POINT" \
         todo="SPEC ARF RMF EVENTS" insts="1 2 3 4 6" tstep=0.05 xgrid="0.5 1.0" gtitype="GTI" psftype="2D_PSF" clobber=yes \
 
 cd $old_wd
