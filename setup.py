568d4dc907e4fcc90dd510fabf958c3b2ae9045a
DaKalt <kald@mpe.mpg.de>
2024-07-18 16:55:11 +0200

    deleted unnecessary model

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.398',
+    version='v1.0.399',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
