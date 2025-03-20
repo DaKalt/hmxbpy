79e52cb25864e438e9d64c9e033f8303015f6589
DaKalt <kald@mpe.mpg.de>
2025-03-20 09:25:31 +0100

    added wider priors for bh PL fit

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'HiMaXBipy/rgb',
 
     ],
-    version='v1.0.452',
+    version='v1.0.453',
     license='MIT',
     description='A python tool to analyse eROSITA data of HMXB',
     author='David M. Kaltenbrunner',
